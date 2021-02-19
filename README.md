6662ce03e06337a96d38cc30c1ba144f369143d9
Jesse Daems <JESSEDAEMS@GMAIL.COM>
2021-02-19 13:17:02 +0100

    wachttijd voor vakken verwijderen dynamisch

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 Een Chrome extensie om gekozen vakken in de SmartSchool Agenda te verbergen, bijvoorbeeld voor wanneer je een bepaald vak niet meevolgt.
 
-In een zeer vroege fase van ontwikkeling.
--- content.js
+++ content.js
@@ -1,16 +1,56 @@
 document.getElementById("navNextOuter").addEventListener("click", loadPageActions, false);
 document.getElementById("navPrevOuter").addEventListener("click", loadPageActions, false);
+kleur = "";
 loadPageActions();
 function loadPageActions() {
-    setTimeout(function () { removeThingies() }, 500);
-}
-function removeThingies(params) {
     chrome.storage.sync.get('color', function (data) {
-        var nodes = document.getElementsByClassName(data.color);
-        Array.from(nodes).forEach(function (element) {
-            if (!element.classList.contains("lessonFormColorExample")) {
-                element.firstElementChild.firstElementChild.remove();
+        if (data.color != "") {
+            kleur = data.color;
+            wachtOpElementen();
+        }
+        else {
+            console.log("Geen kleur geselecteerd. Er wordt dus geen enkel vak verborgen.");
+        }
+    });
+}
+function wachtOpElementen() {
+    slechteElementen = document.getElementsByClassName(kleur);
+    if (slechteElementen.length == 1) {
+        setTimeout(wachtOpElementen, 10);
+    }
+    else {
+        verwijderVakken();
+    }
+}
+function verwijderVakken() {
+    var lesuren = document.getElementsByClassName(kleur);
+    var vakken = [];
+    Array.from(lesuren).forEach(function (element) {
+        if (!element.classList.contains("lessonFormColorExample")) {
+            vak = element.querySelectorAll(".agendaGridBlockContentLeftCourseTitle")[0];
+            if (vak) {
+                vaknaam = vak.innerHTML.split(",")[0];
             }
-        });
+
+            if (!vakken.includes(vaknaam)) {
+                vakken.push(vaknaam);
+            }
+            lesonderwerp = element.querySelectorAll(".agendaGridBlockContentLeftSubject")[0];
+            if (lesonderwerp) {
+                lesonderwerp.remove();
+            }
+            taken = element.querySelectorAll(".agendaGridBlockContentRight")[0];
+            if (taken) {
+                taken.remove();
+            }
+        }
+    });
+    var taken = document.getElementsByClassName("tasksAndMaterials__title");
+    Array.from(taken).forEach(function (element) {
+        vakken.forEach(function (vak) {
+            if (element.innerHTML.includes(vak)) {
+                element.parentElement.parentElement.remove();
+            }
+        })
     });
 }
